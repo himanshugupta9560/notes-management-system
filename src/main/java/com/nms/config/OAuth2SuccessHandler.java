8ae56e278fd8ce58cf7e726a1064a0e13c008c07
himanshugupta9560 <guptahimanshu1287@gmail.com>
2026-08-22 13:59:04 +0530

    Update OAuth2SuccessHandler.java

--- src/main/java/com/nms/config/OAuth2SuccessHandler.java
+++ src/main/java/com/nms/config/OAuth2SuccessHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value; // ✅ यह line जरूर डालो
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,34 +28,25 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final ObjectMapper objectMapper;
     private final OAuth2Service oAuth2Service;
 
-     @Value("${frontend.redirect.url:http://localhost:5173}")
+    @Value("${frontend.redirect.url:http://localhost:5173}")
     private String frontendRedirectUrl;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
-        // here we take token from google
+
         OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
-        // here we extract user from google
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        // here we also take registration id jisme google / facebok jisse bhi login kiya
-        // hot ahe wo likha hota he
         String registrationId = token.getAuthorizedClientRegistrationId();
 
-        // yaha per ham ek aisa method create kar rahe he jo authe service me OAuth2
-        // login ko handler
         ResponseEntity<LoginResponseDto> loginResponseDtoResponseEntity = oAuth2Service
                 .handleOAuth2LoginRequest(oAuth2User, registrationId);
 
-        //ab ham yaha per user ki sari information ko get karenge gmail se
         Map<String, Object> attributes = oAuth2User.getAttributes();
         System.out.println(attributes);
         response.setStatus(loginResponseDtoResponseEntity.getStatusCode().value());
-//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//        response.getWriter().write(objectMapper.writeValueAsString(loginResponseDtoResponseEntity.getBody()));
 
-        // this is different thing
-         String redirectUrl = frontendRedirectUrl + "/callback?token=" 
+        String redirectUrl = frontendRedirectUrl + "/callback?token=" 
                 + loginResponseDtoResponseEntity.getBody().getJwt() 
                 + "&id=" + loginResponseDtoResponseEntity.getBody().getId() 
                 + "&given_name=" + attributes.get("given_name") 
